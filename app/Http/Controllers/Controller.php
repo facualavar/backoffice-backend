7d1f25f634f22401cec78c8884520aec0f180599
Facundo Alavar <facundo.alavar@enviopack.com>
2023-06-09 18:49:40 -0300

    BAC-4 Delete initial laravel controller and add route to CRUD roles in application layer

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Src\Backoffice\Application\Roles\Create\CreateRoleCommand;
+use Src\Backoffice\Application\Roles\Create\RoleCreator;
+use Src\Backoffice\Infrastructure\Roles\Eloquent\EloquentRoleRepository;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    private $repository;
+
+    public function __construct()
+    {
+        $this->repository = new EloquentRoleRepository();
+    }
+
+    public function create(): void
+    {
+        $creator = new RoleCreator($this->repository);
+
+        $crearRol = new CreateRoleCommand($creator);
+
+        $permissions = [
+            [
+                'value' => 'roles_read',
+                'name' => 'Listar Roles',
+            ],
+            [
+                'value' => 'roles_update',
+                'name' => 'Modificar Roles',
+            ],
+        ];
+        $crearRol->__invoke("Admin", $permissions);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Src\Backoffice\Infrastructure\Routing\LaravelRoutingRoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('/roles', LaravelRoutingRoleController::class);
